15df535bf1c6647800e454ca8358780cc91a50c6
loolirer <loolirer@gmail.com>
2026-07-22 15:30:35 -0300

    fix imports

--- turtlebot3_bringup/launch/bringup.launch.py
+++ turtlebot3_bringup/launch/bringup.launch.py
@@ -5,6 +5,8 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
